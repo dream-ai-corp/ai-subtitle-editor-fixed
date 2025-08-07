b40625e0a84b190576fe3c94f1d9e644ba6e7ad5
dream-ai-corp <dream.ai.corp@gmail.com>
2025-08-07 04:20:15 +0200

    Add userManagementBackend/trading_bot/asgi.py

---
+++ userManagementBackend/trading_bot/asgi.py
@@ -0,0 +1,11 @@
+"""
+ASGI config for trading_bot project.
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trading_bot.settings')
+
+application = get_asgi_application() 
